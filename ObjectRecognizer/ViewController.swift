1ab97e94f5558fe57d9eb77e33bf37504efa6185
Tommy Ryanto <tommyryanto@icloud.com>
2018-10-17 12:04:32 +0700

    Initial Commit

---
+++ ObjectRecognizer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ObjectRecognizer
+//
+//  Created by Tommy Ryanto on 17/10/18.
+//  Copyright © 2018 Tommy Ryanto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
